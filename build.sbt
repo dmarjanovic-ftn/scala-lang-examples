72846af21735fa56852bf84fb96624ccc2a83e8c
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-07-08 03:47:19 +0200

    Week 1 Programming Assignment Exercise 1 and 2 initial solution.

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scala-lang-examples"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/week01/Recursion.scala
@@ -0,0 +1,60 @@
+package week01
+
+object Recursion {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+
+    println("\nParentheses Balancing")
+    println(balance("(if (zero? x) max (/ 1 x))".toList))
+    println(balance("I told him (that it’s not (yet) done). (But he wasn’t listening)".toList))
+    println(balance(":-)".toList))
+    println(balance("())(".toList))
+
+  }
+
+  /**
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (r < 0 || c < 0)
+      0
+    else if (r == 0 && c == 0)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    var parentheses = 0
+
+    def balanceHlp(chars: List[Char]) : Boolean = {
+      if (parentheses < 0)
+        false
+      else if (chars.isEmpty)
+        parentheses == 0
+      else {
+        if (chars.head == '(')
+          parentheses = parentheses + 1
+        else if (chars.head == ')')
+          parentheses = parentheses - 1
+
+        balanceHlp(chars.tail)
+      }
+    }
+
+    balanceHlp(chars)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
